d2575a9be8221ea60e49c9370ea592786b232b68
Fabio Zanini <fabio.zanini@fastmail.fm>
2023-03-22 13:15:03 +1100

    Start working on the Python API

---
+++ Python/setup.py
@@ -0,0 +1,18 @@
+"""
+Setup script for atlasapprox Python package
+"""
+import pathlib
+from setuptools import setup, find_packages
+
+
+# version
+version_file = pathlib.Path(__file__).parent / "VERSION"
+with open(version_file) as f:
+    version = f.read()
+
+
+setup(
+    name="atlasapprox",
+    version=version,
+    packages=find_packages(),
+)
