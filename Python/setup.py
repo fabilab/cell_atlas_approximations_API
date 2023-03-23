fd117ff3674212115d8f73f983802e326ef63692
Fabio Zanini <fabio.zanini@fastmail.fm>
2023-03-23 18:27:36 +1100

    Python 0.1.2 with better long description in markdown

--- Python/setup.py
+++ Python/setup.py
@@ -11,9 +11,23 @@ with open(version_file) as f:
     version = f.read().rstrip('\n')
 
 
-long_description = """# Python interface to cell atlas approximations.
+long_description = """[![Documentation Status](https://readthedocs.org/projects/atlasapprox/badge/?version=latest)](https://apidocs.atlasapprox.org/en/latest/?badge=latest)
 
-**Documentation**: https://atlasapprox.readthedocs.io
+Python interface to cell atlas approximations
+=============================================
+Cell atlases such as Tabula Muris and Tabula Sapiens are multi-organ single cell omics data sets describing entire organisms. A cell atlas approximation is a lossy and lightweight compression of a cell atlas that can be streamed via the internet.
+
+This project enables biologists, doctors, and data scientist to quickly find answers for questions such as:
+
+- *What is the expression of a specific gene in human lung?*
+- *What are the marker genes of a specific cell type in mouse pancreas*?
+- *What fraction of cells (of a specific type) express a gene of interest?*
+
+In addition to this interface, these questions can be asked in R or in a language agnostic manner using the REST API. See the documentation for more info.
+
+
+**Documentation**: https://atlasapprox.readthedocs.io/en/latest/python/index.html
+
 **Development**: https://github.com/fabilab/cell_atlas_approximations_API
 """
 
