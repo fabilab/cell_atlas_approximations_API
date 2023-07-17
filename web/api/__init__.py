328aaf521f752567f9c5c9bab7282bd74f6e1bcb
Fabio Zanini <fabio.zanini@fastmail.fm>
2023-07-17 15:02:20 +1000

    Slight improvement in file structure for API v1

--- web/api/__init__.py
+++ web/api/__init__.py
@@ -1,45 +1,11 @@
 """Main module for API objects and endpoints"""
+from api.v1 import api_dict as api_dict_v1
 
-from api.v1.endpoints import get_api_endpoint
-from api.v1.objects import (
-    MeasurementTypes,
-    Organisms,
-    Organs,
-    Celltypes,
-    Features,
-    Average,
-    FractionDetected,
-    Markers,
-    DataSources,
-    HighestMeasurement,
-    SimilarFeatures,
-    SimilarCelltypes,
-    CelltypeXOrgan,
-    CelltypeLocation,
-)
 
 __all__ = (
     "api_dict",
 )
 
 api_dict = {
-    "v1": {
-        "endpoint_handler": get_api_endpoint,
-        "objects": {
-            "measurement_types": MeasurementTypes,
-            "organisms": Organisms,
-            "organs": Organs,
-            "features": Features,
-            "celltypes": Celltypes,
-            "average": Average,
-            "fraction_detected": FractionDetected,
-            "markers": Markers,
-            "highest_measurement": HighestMeasurement,
-            "similar_features": SimilarFeatures,
-            "similar_celltypes": SimilarCelltypes,
-            "celltypexorgan": CelltypeXOrgan,
-            "celltype_location": CelltypeLocation,
-            "data_sources": DataSources,
-        }
-    }
+    "v1": api_dict_v1,
 }
---
+++ web/api/v1/__init__.py
@@ -0,0 +1,42 @@
+"""Main module for API v1"""
+from api.v1.endpoints import get_api_endpoint
+from api.v1.objects import (
+    MeasurementTypes,
+    Organisms,
+    Organs,
+    Celltypes,
+    Features,
+    Average,
+    FractionDetected,
+    Markers,
+    DataSources,
+    HighestMeasurement,
+    SimilarFeatures,
+    SimilarCelltypes,
+    CelltypeXOrgan,
+    CelltypeLocation,
+)
+
+__all__ = (
+    "api_dict",
+)
+
+api_dict = {
+    "endpoint_handler": get_api_endpoint,
+    "objects": {
+        "measurement_types": MeasurementTypes,
+        "organisms": Organisms,
+        "organs": Organs,
+        "features": Features,
+        "celltypes": Celltypes,
+        "average": Average,
+        "fraction_detected": FractionDetected,
+        "markers": Markers,
+        "highest_measurement": HighestMeasurement,
+        "similar_features": SimilarFeatures,
+        "similar_celltypes": SimilarCelltypes,
+        "celltypexorgan": CelltypeXOrgan,
+        "celltype_location": CelltypeLocation,
+        "data_sources": DataSources,
+    }
+}
