1e2b0fe2450ab884a0f5f1600364ad6152b0bf81
Fabio Zanini <fabio.zanini@fastmail.fm>
2024-09-02 17:29:37 +1000

    deploy scripts via twine

---
+++ Python/deploy.sh
@@ -0,0 +1,1 @@
+.venv/bin/twine upload dist/atlasapprox-0.2.2* -u __token__ -p $(<pypi.token)
---
+++ Python/deploy_test.sh
@@ -0,0 +1,1 @@
+.venv/bin/twine upload -r testpypi dist/atlasapprox-0.2.2* -u __token__ -p $(<testpypi.token)
